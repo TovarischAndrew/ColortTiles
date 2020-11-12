686d30691e6934702a3bcd9d412f08c8bc092e06
Shcherbinina Alina <alinkash@gmail.com>
2020-11-12 18:58:12 +0800

    redo

--- app/src/main/java/com/example/color2/MainActivity.java
+++ app/src/main/java/com/example/color2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.colorstuff;
+package com.example.color2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/com/example/color2/NewClass.java
+++ app/src/main/java/com/example/color2/NewClass.java
@@ -1,17 +1,18 @@
-package com.example.colorstuff;
+package com.example.color2;
 
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.Nullable;
+        import android.content.Context;
+        import android.graphics.Canvas;
+        import android.graphics.Color;
+        import android.graphics.Paint;
+        import android.graphics.Rect;
+        import android.util.AttributeSet;
+        import android.view.MotionEvent;
+        import android.view.View;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+        import androidx.annotation.Nullable;
 
 class Tile {
     int color;
@@ -38,7 +39,8 @@ class Tile {
 }
 
 public class NewClass extends View {
-    Tile[][] tiles = new Tile[2][2];
+    int outline = 5;
+    Tile[][] tiles = new Tile[4][4];
     boolean generating = true;
     int width, height; // ширина и высота канвы
 
@@ -58,8 +60,8 @@ public class NewClass extends View {
         height = canvas.getHeight();
 
 
-        int tile_width = width / 2;
-        int tile_height = height / 2;
+        int tile_width = width / 4;
+        int tile_height = height / 4;
         // 2) отрисовка плиток
         // задать цвет можно, используя кисть
         Paint light = new Paint();
@@ -71,8 +73,8 @@ public class NewClass extends View {
         dark.setStyle(Paint.Style.FILL);
 
 
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
 
                 int left = j * tile_width;
                 int top = i * tile_height;
@@ -80,7 +82,7 @@ public class NewClass extends View {
                 int bottom = top + tile_height;
 
                 Rect tile = new Rect();
-                tile.set(left, top, right, bottom);
+                tile.set(left + outline, top + outline, right - outline, bottom - outline);
 
                 int color;
                 if (generating) {
@@ -117,16 +119,16 @@ public class NewClass extends View {
         // 4) определить тип события
         if (event.getAction() == MotionEvent.ACTION_DOWN)// палец коснулся экрана
         {
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
 
                     if (tiles[i][j].left < x && tiles[i][j].right > x && tiles[i][j].top < y && tiles[i][j].bottom > y) { //neighbours
 // 5) определить, какой из плиток коснулись
                         // изменить её цвет на противоположный
                         int k = i, m = j;
 
-                        for (int ii = 0; ii < 2; ii++) { //change colour
-                            for (int jj = 0; jj < 2; jj++) {
+                        for (int ii = 0; ii < 4; ii++) { //change colour
+                            for (int jj = 0; jj < 4; jj++) {
 
                                 if (ii == k || jj == m) {
                                     if (tiles[ii][jj].getColor() == 1)
@@ -143,22 +145,22 @@ public class NewClass extends View {
             //check if won
             int sum = 0;
 
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 4; j++) {
                     sum += tiles[i][j].getColor(); // get colors returns 0 or 1
                 }
             }
             int color = 0;
 
             // 6) проверить, не выиграли ли вы (все плитки одного цвета)
-            if (sum == 4 || sum == 0) { // if one color
+            if (sum == 4 * 4 || sum == 0) { // if one color
                 Toast toast = Toast.makeText(getContext(), // text view crashed my app
                         "You won!", Toast.LENGTH_SHORT);
                 toast.show();
             }
 
         }
-            invalidate(); // заставляет экран перерисоваться
-            return true;
-        }
+        invalidate(); // заставляет экран перерисоваться
+        return true;
     }
+}
